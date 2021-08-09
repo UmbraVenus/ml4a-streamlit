c17ebf227425d5063c8a114b4641e4a60316fa29
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2021-08-09 09:16:38 -0700

    rearranged

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import reference
 
 app = MultiApp()
 
-app.add_app("Text to Song", music.app)
 app.add_app("Text to Image", texttoimage.app)
 app.add_app("Recommendation from Deviant Art", recommendation.app)
+app.add_app("Text to Song", music.app)
 #app.add_app("Reference", reference.app)
 
 app.run()
