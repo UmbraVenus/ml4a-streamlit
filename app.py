006311a75c655c02e593942df44bb8b749dba8d2
UmbraVenus <60438078+UmbraVenus@users.noreply.github.com>
2021-09-04 15:28:41 -0700

    adjusted donate

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ app.add_app("Recommendation from Deviant Art", recommendation.app)
 app.add_app("Reference", reference.app)
 
 st.sidebar.caption("Feeling Philanthropic? Currently need $50 a month for website maintenance for underserved artists. Any amount helps, thank you! :blush:")
-st.sidebar.markdown('<form action="https://www.paypal.com/donate" method="post" target="_top" class="donate"> <input type="hidden" name="hosted_button_id" value="8RDNKFK7RKQRQ" /> <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" /><img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" /></form>', unsafe_allow_html=True)
+# st.sidebar.markdown('<form action="https://www.paypal.com/donate" method="post" target="_top" class="donate"> <input type="hidden" name="hosted_button_id" value="8RDNKFK7RKQRQ" /> <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" /><img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" /></form>', unsafe_allow_html=True)
+
+link = "[Donate](https://www.paypal.com/donate?hosted_button_id=NE79CMLKUW6HL)"
+st.sidebar.markdown(link, unsafe_allow_html=True)
 
 app.run()
